aec11882a030c3ef9aff41988c422a9d02c919f6
Yamil Moreno <yamilmoreno_isc@hotmail.com>
2026-05-23 13:09:19 -0600

    Aprendiendo python

---
+++ hola.py
@@ -0,0 +1,16 @@
+nombre = "Yamil"		 #texto, se llama String
+edad = 23			 #número entero, se llama Integer
+altura = 1.80			 #número decimal, se llama Float
+estudiante = True		 #verdadero o falso, se llama Boolean
+ciudad = "Minatitlan"
+
+print(f"Mi nombre es {nombre}")
+print(f"Tengo {edad} años")
+print(f"Mido {altura}")
+print(f"Soy estudiante {estudiante}")
+print(f"Vivo en {ciudad}")
+
+año_actual = 2026
+año_nacimiento = 1998
+edad_calculada = año_actual - año_nacimiento
+print(f"Si naciste en {año_nacimiento} tienes actualmente {edad_calculada} años")
