5190a4ee752a050e833c0fb89020f708934c0f3f
Yamil Moreno <yamilmoreno_isc@hotmail.com>
2026-05-23 13:26:49 -0600

    Se agregaron lineas en los archivos

---
+++ operaciones.py
@@ -0,0 +1,15 @@
+suma = 10 + 5
+resta = 10 - 5
+multiplicacion = 10 * 5
+division = 10 / 5
+
+print(f"Cuanto es 10 + 5?: {suma}")
+print(f"Cuanto es 10 - 5?: {resta}")
+print(f"Cuanto es 10 * 5?: {multiplicacion}")
+print(f"Cuanto es 10 / 5?: {division}")
+print(f"División normal: {10 / 5}")
+print(f"Division entera: {10 // 5}")
+
+print(f"10 % 3 = {10 % 3}")
+print(f"10 % 2 = {10 % 2}")
+print(f"11 % 2 = {11 % 2}")
