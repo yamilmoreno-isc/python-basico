403e67456cd30f63aaf56e302692a00d0fcc9e50
Yamil Moreno <yamilmoreno_isc@hotmail.com>
2026-05-23 16:30:16 -0600

    Agreando lineas

---
+++ usuario.py
@@ -0,0 +1,8 @@
+nombre = input("¿Cómo te llamas? ")
+edad = input("¿Cuál es tu edad? ")
+ciudad = input("¿En que ciudad vives? ")
+print(f"Hola {nombre}!")
+print(f"Tienes {edad} años y eres de la ciudad de {ciudad}! ")
+año_nacimiento = int(input("¿En qué año naciste? "))
+edad_calculada = 2026 - año_nacimiento
+print(f"Tienes {edad_calculada} años")
